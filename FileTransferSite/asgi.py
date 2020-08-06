701d6636b998132a56820d2e474bc37dc402addf
Rick McIlraith <rick.mcilraith@wearesponge.com>
2020-08-06 09:55:38 +0100

    Updated leftover filename occurences

--- FileTransferSite/asgi.py
+++ FileTransferSite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for softwareEngineerExercise project.
+ASGI config for FileTransferSite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'softwareEngineerExercise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FileTransferSite.settings')
 
 application = get_asgi_application()
--- FileTransferSite/urls.py
+++ FileTransferSite/urls.py
@@ -1,4 +1,4 @@
-"""softwareEngineerExercise URL Configuration
+"""FileTransferSite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
